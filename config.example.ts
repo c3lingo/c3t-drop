193551a9727d6e091ede89b22fedc30b6cfd6a97
sebalis <sebalis@gmx.net>
2021-12-23 07:17:32 +0100

    added eventName to config.example.ts

--- config.example.ts
+++ config.example.ts
@@ -1,5 +1,7 @@
 import { resolve } from 'path';
 
+export const eventName: string = "rC3 2021";
+
 export const readCredentials: { [username: string]: string } = {
   c3lingo: '39*Aug6{Pe6F=976EcV}9Cf{eW3v4v',
   foo: 'bar',
